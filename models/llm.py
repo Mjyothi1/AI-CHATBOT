0d7904e19e2baa80a56592469f35a56a197c30d6
Mjyothi1 <159994492+Mjyothi1@users.noreply.github.com>
2026-03-14 09:51:36 +0530

    Update API key in get_chatgroq_model function
    Replaced GROQ_API_KEY variable with a hardcoded API key.

--- models/llm.py
+++ models/llm.py
@@ -4,7 +4,7 @@ from config.config import GROQ_API_KEY
 def get_chatgroq_model():
     try:
         model = ChatGroq(
-            api_key=GROQ_API_KEY,
+            api_key="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",
             model="llama-3.1-8b-instant"
         )
         return model
